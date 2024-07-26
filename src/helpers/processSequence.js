fdc6536cf84e1e0c54e74d31f0f26f686f20e53c
Alisher Abylkassimov <abylkassimov.alisher@gmail.com>
2024-07-26 15:30:13 +0500

    task completed

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,69 @@
 /**
  * @file Домашка по FP ч. 2
- *
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- *
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- *
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from "../tools/api";
+import * as R from 'ramda';
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+    const log = R.tap(writeLog);
+    console.log("value", value)
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+    const validateLength = R.both(
+        R.compose(R.lt(R.__, 10), R.length),
+        R.compose(R.gt(R.__, 2), R.length)
+    );
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+    const isPositiveNumber = R.both(
+        R.compose(R.gt(R.__, 0), parseFloat),
+        R.compose(R.not, R.equals(NaN), parseFloat)
+    );
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+    const validateFormat = R.test(/^[0-9.]+$/);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+    const validate = R.ifElse(
+        R.allPass([validateLength, isPositiveNumber, validateFormat]),
+        R.identity,
+        () => handleError("Validation Error")
+    );
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+
+    const toNumberAndRound = R.compose(
+        R.tap(log),
+        Math.round,
+        parseFloat
+    );
+    const toBinary = number =>
+        api.get('https://api.tech/numbers/base', { from: 10, to: 2, number })
+            .then(R.prop('result'))
+    
+    const getAnimal = id => 
+        api.get(`https://animals.tech/${id}`, {})
+        .   then(R.prop('result'))
+            
+    const process = R.pipe(
+        log,
+        validate,
+        toNumberAndRound,
+        toBinary,
+        R.andThen(R.pipe(
+            R.length,
+            log,
+            R.pipe(
+                R.curry(Math.pow)(R.__, 2),
+                log,
+                R.modulo(R.__, 3),
+                log,
+            ),
+        )),
+        R.andThen(
+            getAnimal,
+        ),
+        R.andThen(handleSuccess)
+    );
+    
+    Promise.resolve(process(value)).catch(handleError);
+};
 
 export default processSequence;
